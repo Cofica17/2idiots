57f33ce9d1e97d8ad47ebd7c0f5be41e9063b8ac
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-08-22 09:27:11 +0200

    Added Walk movement, adjusted jump strength from walking state, NOTE: Add 'walk' to your input map

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,11 +5,13 @@ onready var model = $Model
 onready var camera = $Camera
 
 export var character_model:PackedScene = preload("res://assets/characthers/models/fbx/people_unity/king.fbx") setget set_character_model
-export var speed = 10
+export var running_speed = 10
+export var walking_speed = 4
 export var stopping_speed = 0.1
 export var turn_angle = 0.1
 export var gravity = Vector3(0, -35, 0)
 export var jump_strength = 15
+export var jump_strength_walk_modifier = 0.7
 
 var velocity = Vector3.ZERO
 
@@ -33,15 +35,24 @@ func _physics_process(delta):
 
 func handle_jump() -> void:
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = jump_strength
+		if Input.is_action_pressed("walk"):
+			velocity.y = jump_strength * jump_strength_walk_modifier
+		else:
+			velocity.y = jump_strength
 
 func apply_gravity(delta) -> void:
 	velocity += gravity * delta
 
 func handle_movement() -> void:
 	var vel_y = velocity.y
+	var speed = 0
 	velocity = lerp(velocity, Vector3.ZERO, stopping_speed)
 	
+	if Input.is_action_pressed("walk") and is_on_floor():
+		speed = walking_speed
+	else:
+		speed = running_speed
+		
 	if Input.is_action_pressed("move_forward"):
 		velocity = global_transform.basis.z * speed
 	if Input.is_action_pressed("move_back"):

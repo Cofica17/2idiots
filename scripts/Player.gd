a948d92a75975aa2e962fbb0612580ba91e8b88c
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-08-22 08:22:18 +0200

    Fixed multiple jump issue, added return type to methods

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,15 +5,15 @@ onready var model = $Model
 onready var camera = $Camera
 
 export var character_model:PackedScene = preload("res://assets/characthers/models/fbx/people_unity/king.fbx") setget set_character_model
-export var speed = 50
+export var speed = 10
 export var stopping_speed = 0.1
 export var turn_angle = 0.1
-export var gravity = Vector3(0, -30, 0)
-export var jump_strength = 20
+export var gravity = Vector3(0, -35, 0)
+export var jump_strength = 15
 
 var velocity = Vector3.ZERO
 
-func _ready():
+func _ready() -> void:
 	pass
 
 func set_character_model(v) -> void:
@@ -25,20 +25,20 @@ func set_character_model(v) -> void:
 		child.queue_free()
 	$Model.add_child(character_model.instance())
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	apply_gravity(delta)
 	handle_movement()
 	handle_jump()
 	velocity = move_and_slide(velocity, Vector3.UP)
 
-func handle_jump():
-	if Input.is_action_pressed("jump"):
+func handle_jump() -> void:
+	if Input.is_action_pressed("jump") and self.is_on_floor():
 		velocity.y = jump_strength
 
-func apply_gravity(delta):
+func apply_gravity(delta) -> void:
 	velocity += gravity * delta
 
-func handle_movement():
+func handle_movement() -> void:
 	var vel_y = velocity.y
 	velocity = lerp(velocity, Vector3.ZERO, stopping_speed)
 	

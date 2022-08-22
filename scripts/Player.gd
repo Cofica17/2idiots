42e9bc4dafd2cfa9b6703d97d54f8b99e7a064fd
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-08-22 10:23:16 +0200

    Added double jump

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,7 +4,7 @@ class_name Player
 onready var model = $Model
 onready var camera = $Camera
 
-export var character_model:PackedScene = preload("res://assets/characthers/models/fbx/people_unity/king.fbx") setget set_character_model
+export var character_model:PackedScene = preload("res://assets/characthers/models/godot_models/king.tscn") setget set_character_model
 export var running_speed = 10
 export var walking_speed = 4
 export var stopping_speed = 0.1
@@ -12,6 +12,7 @@ export var turn_angle = 0.1
 export var gravity = Vector3(0, -35, 0)
 export var jump_strength = 15
 export var jump_strength_walk_modifier = 0.7
+var is_double_jumping = false
 
 var velocity = Vector3.ZERO
 
@@ -35,10 +36,20 @@ func _physics_process(delta):
 
 func handle_jump() -> void:
 	if Input.is_action_pressed("jump") and is_on_floor():
+		is_double_jumping = false
+		if Input.is_action_pressed("walk"):
+			velocity.y = jump_strength * jump_strength_walk_modifier
+		else:
+			velocity.y = jump_strength 
+		return
+		
+	elif Input.is_action_just_pressed("jump") and !is_double_jumping and !is_on_floor():
+		is_double_jumping = true
 		if Input.is_action_pressed("walk"):
 			velocity.y = jump_strength * jump_strength_walk_modifier
 		else:
 			velocity.y = jump_strength
+		
 
 func apply_gravity(delta) -> void:
 	velocity += gravity * delta

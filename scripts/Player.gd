21a4402e6072c19b00906f5be36e02f2c4134d3f
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-08-22 18:28:32 +0200

    Adjusted double jumping

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,12 +7,15 @@ onready var camera = $Camera
 export var character_model:PackedScene = preload("res://assets/characthers/models/godot_models/king.tscn") setget set_character_model
 export var running_speed = 10
 export var walking_speed = 4
-export var stopping_speed = 0.1
+export var stopping_speed_ground = 0.1
+export var stopping_speed_ground_air = 0.02
 export var turn_angle = 0.05
 export var gravity = Vector3(0, -35, 0)
 export var jump_strength = 15
 export var jump_strength_walk_modifier = 0.7
+export var backward_speed_modifier = 0.5
 var is_double_jumping = false
+var is_walk_jumping = false
 
 var velocity = Vector3.ZERO
 
@@ -38,26 +41,33 @@ func handle_jump() -> void:
 	if Input.is_action_pressed("jump") and is_on_floor():
 		is_double_jumping = false
 		if Input.is_action_pressed("walk"):
+			is_walk_jumping = true
 			velocity.y = jump_strength * jump_strength_walk_modifier
 		else:
+			is_walk_jumping = false
 			velocity.y = jump_strength 
 		return
 		
 	elif Input.is_action_just_pressed("jump") and !is_double_jumping and !is_on_floor():
 		is_double_jumping = true
-		if Input.is_action_pressed("walk"):
+		if is_walk_jumping:
 			velocity.y = jump_strength * jump_strength_walk_modifier
+			print("Double Walk Jump")
 		else:
 			velocity.y = jump_strength
+			print("Double Run Jump")
 		
-
 func apply_gravity(delta) -> void:
 	velocity += gravity * delta
 
 func handle_movement() -> void:
 	var vel_y = velocity.y
+	print(velocity.length())
 	var speed = 0
-	velocity = lerp(velocity, Vector3.ZERO, stopping_speed)
+	if is_on_floor():
+		velocity = lerp(velocity, Vector3.ZERO, stopping_speed_ground)
+	else:
+		velocity = lerp(velocity, Vector3.ZERO, stopping_speed_ground_air)
 	
 	if Input.is_action_pressed("walk") and is_on_floor():
 		speed = walking_speed
@@ -75,3 +85,4 @@ func handle_movement() -> void:
 		rotate_y(-turn_angle)
 	
 	velocity.y = vel_y
+

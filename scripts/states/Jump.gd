ae90f4a58808de82237962176438065d494b432d
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-08-25 14:00:15 +0200

    Removed unneededp rint, left prints when entering state

--- scripts/states/Jump.gd
+++ scripts/states/Jump.gd
@@ -12,7 +12,6 @@ func _physics_process():
 	if not player.is_jumping:
 		player.is_jumping = true
 		player.velocity.y = player.jump_strength
-		print(player.velocity.length())
 		
 	elif player.velocity.y < 0:
 		locomotion.set_fall_state()

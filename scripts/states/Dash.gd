3df2f6f6b3d8dd1d9dfd4c88843300874a3e2d5a
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-08-25 14:27:45 +0200

    Linting

--- scripts/states/Dash.gd
+++ scripts/states/Dash.gd
@@ -8,10 +8,12 @@ func enter():
 
 func _physics_process():
 	._physics_process()
+	
 	if not player.is_dashing:
 		player.is_dashing = true
 		player.stamina -= player.required_dash_stamina
 		move_forward(player.dash_move_forward)
+		
 	if player.is_dashing:
 		player.velocity = lerp(player.velocity, Vector3.ZERO, player.dash_stopping_speed)
 		if player.velocity.length() < player.dash_idle_treshold:
@@ -20,7 +22,6 @@ func _physics_process():
 				locomotion.set_walk_state()
 			else:
 				locomotion.set_idle_state()
-
 
 func exit():
 	.exit()

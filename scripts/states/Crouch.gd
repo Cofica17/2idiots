ba42398050365bdaa74e939a3817e33e893f9ed0
Cofica17 <63316032+Cofica17@users.noreply.github.com>
2022-08-26 16:10:51 +0200

    added crouch idle

--- scripts/states/Crouch.gd
+++ scripts/states/Crouch.gd
@@ -3,7 +3,7 @@ class_name Crouch
 
 func enter():
 	.enter()
-	play_animation("crouch")
+	play_animation("crouch_idle")
 	print("Crouch")
 
 func _physics_process():
@@ -13,10 +13,13 @@ func _physics_process():
 		locomotion.set_idle_state()
 		
 	if is_forward():
+		play_animation("crouch")
 		move_forward(player.crouch_speed)
 	elif is_back():
+		play_animation("crouch")
 		move_back(player.crouch_speed)
 	else:
+		play_animation("crouch_idle")
 		player.velocity = lerp(player.velocity, Vector3.ZERO, player.stopping_speed_ground)
 
 func exit():

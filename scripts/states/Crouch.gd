94ea3aff803d6ca7b51f636062cfafef4a181395
luka <luka.majstorovic321@gmail.com>
2022-08-30 21:13:34 +0200

    Fixed crouch issue

--- scripts/states/Crouch.gd
+++ scripts/states/Crouch.gd
@@ -4,6 +4,7 @@ class_name Crouch
 func enter():
 	.enter()
 	min_recoil = 0
+	max_recoil = 4
 	play_animation("crouch_idle")
 
 func get_class() -> String: return "Crouch"
@@ -17,17 +18,20 @@ func _physics_process():
 		locomotion.set_idle_state()
 		
 	if is_forward():
-		max_recoil = 4
+		min_recoil = 2
+		max_recoil = 6
 		play_animation("crouch")
 		move_forward(player.crouch_speed)
 	elif is_back():
-		max_recoil = 4
+		min_recoil = 2
+		max_recoil = 6
 		play_animation("crouch")
 		move_back(player.crouch_speed)
 	else:
 		if locomotion.state_machine.get_current_node() != "crouch_idle":
 			play_animation("crouch_idle")
 			min_recoil = 0
+			max_recoil = 4
 		player.velocity = lerp(player.velocity, Vector3.ZERO, player.stopping_speed_ground)
 
 func exit():

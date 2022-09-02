fa3b9a2a7c56f626f4d122539e3902e952dfdb6c
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-09-02 21:48:40 +0200

    Adjusted crosshair

--- scripts/Crosshair.gd
+++ scripts/Crosshair.gd
@@ -6,23 +6,37 @@ onready var min_recoil = player.player_locomotion.state.min_recoil
 onready var max_recoil = player.player_locomotion.state.max_recoil
 
 export var crosshair_reduction_time = 0.3
-export var crosshair_offset = 0.01
+export var crosshair_offset = 0.015
 
 var current_recoil = 1
 var time = 0
+var recoil_values_dict = {
+	0:0.04,
+	1:0.05,
+	2:0.06,
+	3:0.07,
+	4:0.08,
+	5:0.09,
+	6:0.10,
+	7:0.11,
+	8:0.12,
+	9:0.13,
+}
 
 func _ready():
 	player.connect("attacked", self, "_on_player_attacked")
 
-	
 func _physics_process(delta):
 	min_recoil = player.player_locomotion.state.min_recoil
 	max_recoil = player.player_locomotion.state.max_recoil
-	print(crosshair.rect_scale)
 	adjust_recoil(delta)
-	var v = Vector2(current_recoil, current_recoil)
-	crosshair.rect_scale = v
+	adjust_crosshair()
+
+func adjust_crosshair() -> void:
+	var scale = recoil_values_dict.get(current_recoil)
+	var scale_vector = Vector2(scale, scale)
 	var viewport_size = get_viewport_rect().size
+	crosshair.scale = scale_vector
 	crosshair.set_global_position(Vector2(viewport_size.x / 2, viewport_size.y / 2 + crosshair_offset * viewport_size.y))
 	
 func adjust_recoil(delta) -> void:
